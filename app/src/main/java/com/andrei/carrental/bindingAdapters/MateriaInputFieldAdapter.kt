9f03718cfba8b6179e6cb467ad16341e12f996cd
Andrei Avram <avramandreitiberiu@gmail.com>
2021-01-28 11:40:01 +0000

    -added custom adapter for TextInputLayout

--- app/src/main/java/com/andrei/carrental/bindingAdapters/MateriaInputFieldAdapter.kt
+++ app/src/main/java/com/andrei/carrental/bindingAdapters/MateriaInputFieldAdapter.kt
@@ -1,2 +1,16 @@
 package com.andrei.carrental.bindingAdapters
 
+import androidx.databinding.BindingAdapter
+import com.google.android.material.textfield.TextInputLayout
+
+@BindingAdapter("errorText")
+fun bindErrorTextToField(textInputLayout: TextInputLayout, text: String?){
+   if(!text.isNullOrBlank())   {
+       textInputLayout.apply {
+           error = text
+           isErrorEnabled = true
+       }
+   }else{
+        textInputLayout.isErrorEnabled = false
+   }
+}
