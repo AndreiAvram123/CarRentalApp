cde4581937eac17e0f6660ba0fff2ad645624066
Andrei Avram <avramandreitiberiu@gmail.com>
2021-02-17 14:03:19 +0000

    modified observable chat

--- app/src/main/java/com/andrei/carrental/entities/ObservableChat.kt
+++ app/src/main/java/com/andrei/carrental/entities/ObservableChat.kt
@@ -3,7 +3,8 @@ package com.andrei.carrental.entities
 import androidx.lifecycle.LiveData
 import com.andrei.engine.DTOEntities.MessageDTO
 
-data class Chat(
+data class ObservableChat(
+    val id:Long,
     val friend:User,
     val isUserOnline:LiveData<Boolean>,
     val lastMessageDTO:LiveData<Message>
